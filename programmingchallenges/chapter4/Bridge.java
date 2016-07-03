9f976997d05d7c1fac0a630aee008063dc928aa4
bSkwared <gh@truebearsfan.com>
2016-07-03 09:39:33 -0500

    changed some ordering; works same

--- programmingchallenges/chapter4/Bridge.java
+++ programmingchallenges/chapter4/Bridge.java
@@ -3,7 +3,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Bridge {
+class Main {
 
     public static void main (String[] args) {
 
@@ -36,20 +36,28 @@ public class Bridge {
                    time += possA;
 
                     order.addLast(ppl[0]);
-                    order.addLast(ppl[amntToCross - 1]);
-                    order.addLast(ppl[0]);
-                    order.addLast(ppl[0]);
                     order.addLast(ppl[amntToCross - 2]);
+                    order.addLast(-1);
                     order.addLast(ppl[0]);
+                    order.addLast(-1);
+                    order.addLast(ppl[0]);
+                    order.addLast(ppl[amntToCross - 1]);
+                    order.addLast(-1);
+                    order.addLast(ppl[0]);
+                    order.addLast(-1);
                 } else {
                     time += possB;
 
                     order.addLast(ppl[0]);
                     order.addLast(ppl[1]);
+                    order.addLast(-1);
                     order.addLast(ppl[0]);
+                    order.addLast(-1);
                     order.addLast(ppl[amntToCross - 2]);
                     order.addLast(ppl[amntToCross - 1]);
+                    order.addLast(-1);
                     order.addLast(ppl[1]);
+                    order.addLast(-1);
 
                 }
                 amntToCross -= 2;
@@ -59,23 +67,44 @@ public class Bridge {
                 case 3:
                     time += ppl[0] + ppl[1] + ppl[2];
                     order.addLast(ppl[0]);
-                    order.addLast(ppl[2]);
-                    order.addLast(ppl[0]);
-                    order.addLast(ppl[0]);
                     order.addLast(ppl[1]);
+                    order.addLast(-1);
+                    order.addLast(ppl[0]);
+                    order.addLast(-1);
+                    order.addLast(ppl[0]);
+                    order.addLast(ppl[2]);
+                    order.addLast(-1);
                     break;
                 case 2:
                     time += ppl[1];
                     order.addLast(ppl[0]);
                     order.addLast(ppl[1]);
+                    order.addLast(-1);
                     break;
                 case 1:
                     time += ppl[0];
                     order.addLast(ppl[0]);
+                    order.addLast(-1);
                     break;
             }
 
             System.out.println(time);
+
+            while (order.size() > 0) {
+               int item = order.removeFirst();
+               if (item == -1) {
+                   System.out.print('\n');
+               } else {
+                   System.out.print(item);
+                   if (order.size() > 0 && order.peek() != -1) {
+                       System.out.print(' ');
+                   }
+               }
+            }
+
+
+
+            System.out.println();
 
 
 
