02b2b1f2dfd387999ae588444504ed5cd42e5f8b
bSkwared <gh@truebearsfan.com>
2016-07-03 08:03:36 -0500

    added bridge skeleton

---
+++ programmingchallenges/chapter4/Bridge.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bridge {
+
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int cases = scan.nextInt();
+
+        for (int ctr = 0; ctr < cases; ++ctr) {
+            int n = scan.nextInt();
+
+            int[] ppl = new int[n];
+            for (int i = 0; i < n; ++i) {
+                ppl[i] = scan.nextInt();
+            }
+
+            
+
+        }
+    }
+}
